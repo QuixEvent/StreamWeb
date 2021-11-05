92ac0589f49c046df69bcaa142582363ce9f0309
QuixEvent <73504409+QuixEvent@users.noreply.github.com>
2021-11-05 22:18:36 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Testovací stránka QuixEvent</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+	<h1>Testovací stránka QuixEvent</h1>
+<section>
+	<div class="form-align">
+  <div class="form-response">	
+		<form id='upload_form' action='https://studio49.cz/quix/' method='post' enctype='multipart/form-data'> 
+Nahrajte obrázek : 
+<input type='file' name='Filedata' id='Filedata'> 
+ Email: 
+ <input type='email' name='email'> 
+ Zpráva: 
+<textarea name='message' form='upload_form'></textarea> 
+<input type='submit' value='Odeslat' name='submit'> 
+<input type='hidden' name='pid' value='1236199'> 
+<input type='hidden' name='eventName' value='1634560431770-HAVEL_TEST01'> 
+<input type='hidden' name='weakPassword' value='fotomaster_browse'> 
+<input type='hidden' name='redirect_url' value='https://fotomasterltd.net'> 
+</form>
+		</div>
+	</div>
+</section>
+  <!-- ======= Hero Section ======= -->
+  
+	<section>
+	
+	
+  <div class="mosaic-box">
+	<div id=""
+     class="iframe-box">
+     <iframe class="iframe-in" id="inlineFrameExample" title="Inline Frame Example" src="https://api.fotomasterltd.net/photo-mosaic-booth/interactive//mosaic.php?pid=1236199&event_name=1634560431770-HAVEL_TEST01&columns=44&rows=48&max_tile_size=600&useOrig=true"></iframe>
+  </div>
+	  
+	  </div>
+	</section>
+	<section>
+	<div style="mosaic-box">
+	<div id=""
+     class="iframe-box">
+     <iframe class="iframe-in" id="inlineFrameExample" title="Inline Frame Example" src="https://api.fotomasterltd.net/photo-mosaic-booth/live//presentation.html?clientId=1236199&eventId=1634560431770-HAVEL_TEST01&columns=44&rows=48&focusOption=true" ></iframe>
+  </div>
+  </div>
+	
+	</section>
+		
+      
+<footer>
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>QuixEvent</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        
+        Designed by <a href="https://quix.cz/cs">Adam</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+ <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
